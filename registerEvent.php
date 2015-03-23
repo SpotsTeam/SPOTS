f470a115c882e0290fcdc961901a78318b8f527d
Erik Gabrielsen <egabrielsen@smu.edu>
2015-03-23 02:44:51 -0500

    made register Event page pretty

--- registerEvent.php
+++ registerEvent.php
@@ -1,11 +1,62 @@
-<html>
-<head><title>SPOTS Driver Page</title></head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spots Events Page</title>
 
-        <h2>Register Event</h2>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+    <!-- TipueDrop CSS -->
+    <link href="css/tipuedrop.css" rel="stylesheet">
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="css/leaflet.css"
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+<body>
+        <!-- Fixed Top Navbar -->
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                </button>
+                <a href="index.html">
+                    <img alt="" src="img/spotslogo2.png" class="img-brand"></img>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="navbar-left">
+                
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <button data-toggle="modal" data-target="#myModal" class=" btn-lg btn btn-info" style="outline:none"> 
+                        <i class="fa fa-1x fa-car"></i> My Spots </button></li>
+                    <li> <form action="/SPOTS/loginPage.php" class="inline"><button class="dlink btn btn-lg btn-info"  style="outline:none"> 
+                        <i class="fa fa-1x fa-street-view"> </i>Register </button></form></li>
+                    <li> <form action="/SPOTS/aboutMe.php" class="inline"><button class="dlink btn btn-lg btn-info"  style="outline:none"> 
+                        <i class="fa fa-1x fa-car"> </i>About SPOTS </button></form></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+        <div style="margin-top:80px" class="centered">
+        <h2>Register Event</h2></br></br>
         <form method = "post" action="registerDisplay.php">
-                Event Name: <input type="text" name="eventName" /><br/>
-                Start Date:  <select name="startMonth">
+                <label>Event Name: </label><input type="text" name="eventName" /><br/>
+                <label>Start Date: </label> <select name="startMonth">
                         <option value="01">January</option>
                         <option value="02">February</option>
                         <option value="03">March</option>
@@ -65,7 +116,7 @@
                         <option value="2026">2026</option>
                         <option value="2027">2027</option>
                     </select></br>
-                End Date:  <select name="endMonth">
+                <label>End Date: </label> <select name="endMonth">
                         <option value="01">January</option>
                         <option value="02">February</option>
                         <option value="03">March</option>
@@ -125,7 +176,7 @@
                         <option value="2026">2026</option>
                         <option value="2027">2027</option>
                     </select></br>
-                Category: <select name="category">
+                <label>Category: </label><select name="category">
                         <option value="Football">Football</option>
                         <option value="Basketball">Basketball</option>
                         <option value="Lacrosse">Lacrosse</option>
@@ -140,10 +191,10 @@
                         <option value="Frat Party">Frat Party</option>
                         <option value="Other">Other</option>
                     </select></br>
-                Enter Address:</br>
-                <span style="padding: 0 20px">&nbsp;</span>Street: <input type="text" name="address" /><br/>
-                <span style="padding: 0 20px">&nbsp;</span>City: <input type="text" name="city" /><br/>
-                <span style="padding: 0 20px">&nbsp;</span>State: <select name="state">
+                <label>Enter Address:</label></br>
+                <span style="padding: 0 20px">&nbsp;</span><label>Street: </label><input type="text" name="address" /><br/>
+                <span style="padding: 0 20px">&nbsp;</span><label>City:</label> <input type="text" name="city" /><br/>
+                <span style="padding: 0 20px">&nbsp;</span><label>State: </label><select name="state">
                         <option value="AL">AL</option>
                         <option value="AK">AK</option>
                         <option value="AZ">AZ</option>
@@ -196,8 +247,10 @@
                         <option value="WY">WY</option>
                         <option value="DC">DC</option>
 </select></br>
-                <span style="padding: 0 20px">&nbsp;</span>ZipCode: <input type="text" name="zipcode" /></br>
-                <input type="submit" />
+                <span style="padding: 0 20px">&nbsp;</span><label>ZipCode: </label><input type="text" name="zipcode" /></br>
+                <br><br><br><br><br><br>
+        <button data-toggle="modal" data-target="#myModal" class=" btn-lg btn btn-info" style="outline:none; margin-left: 150px"> 
+                        <i class="fa fa-1x fa-car"></i> Submit </button>
         </form>
 </body>
 
