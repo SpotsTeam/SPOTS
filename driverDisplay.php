ad5cd6b8e05a0a56f2cc4f1bf431f921fcd6e6a0
DejayJD <jdfrancis@smu.edu>
2015-04-10 22:49:09 -0500

    added a bunch of stuff

---
+++ driverDisplay.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Spots Homeowner Page</title>
+
+	<!-- Bootstrap CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+    <!-- TipueDrop CSS -->
+    <link href="css/tipuedrop.css" rel="stylesheet">
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="css/leaflet.css"
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<!-- Fixed Top Navbar -->
+	<div class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                </button>
+                <a href="index.html">
+					<img alt="" src="img/spotslogo2.png" class="img-brand"></img>
+				</a>
+            </div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="navbar-left">
+				
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					
+					<li> <form action="/SPOTS/signin/signIn.php" class="inline"><button class="dlink btn btn-lg btn-info"  style="outline:none"> 
+						<i class="fa fa-1x fa-street-view"> </i>Sign In </button></form></li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Main Body -->	
+
+	<div style="margin-top:80px" class="centered">
+
+	<?php
+		if(isset($_POST['username'])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$fname = $_POST['fname'];
+			$lname = $_POST['lname'];
+			$address = $_POST['address'];
+			$city = $_POST['city'];
+			$state = $_POST['state'];
+			$zipcode = $_POST['zipcode'];
+			$carMake = $_POST['carMake'];
+			$carModel = $_POST['carModel'];
+			$licensePlate = $_POST['licensePlate'];
+			$email = $_POST['email'];
+
+			$servername = "localhost";
+
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				?> <h2><br><br><br>You are now a registered Driver!<br></h2> <?php
+			} else {
+				//go back to sign up page
+				//echo "<h3>$email is an invalid email <br>Please enter valid email</h3>";
+				header("Location: /SPOTS/drivePage.php");
+				exit;
+			}
+
+			//here we're going to use the root because it would be easier
+			//WILL BE CHANGED EVENUTALLY TO A USER
+			$databaseUsername = "spotsuser";
+			$databasePassword = "spots123";
+			$database = "spots";
+
+			global $conn;
+			$conn = mysql_connect($servername, $databaseUsername, $databasePassword);
+
+			// Check connection
+			if (!$conn) 
+			{
+				//if the connection fails then we kill the whole thing
+    			die("Connection failed: " . mysql_error());
+			} else {
+				echo "database successfully connected<br>";
+			}
+
+			mysql_select_db($database);
+
+			$insert = "INSERT INTO Driver (username, fname, lname, email, password, street, city, state, zip) VALUES ('$username', '$fname', '$lname', '$email', '$password', '$address', '$city', '$state', $zipcode)";
+
+			if (mysql_query($insert) === TRUE) {	
+				echo "Driver info entered successfully<br>";
+			} else {
+				echo "Error: " . $insert . "<br>" . mysql_error();
+			}
+
+			$query = mysql_query("SELECT userId FROM Driver where username = '$username'", $conn);
+			$row = mysql_fetch_row($query);
+			
+			$userId = $row[0];
+			$insert2 =  "INSERT INTO Vehicle (licensePlate, carMake, carModel, userId) values ('$licensePlate', '$carMake', '$carModel', $userId)";
+
+			if (mysql_query($insert2) === TRUE) {	
+				echo "Car added to $username<br>";
+			} else {
+				echo "Error: " . $insert2 . "<br>" . mysql_error();
+			}
+
+
+
+			//print up the information of the driver
+		// 	echo "<h1> Welcome Driver $name </h1>";
+		// 	echo "<h2> Your Username is: $username </h2>";
+		// 	echo "<h2> Your email is: $email </h2>";
+		// 	echo "<h2> Address: $address, $city, $state, $zipcode</h2>";
+		// 	echo "<h2> Your car is a $carModel and your license plate is $licensePlate";
+		}
+	?>
+</div>
+</body>
+</html>
