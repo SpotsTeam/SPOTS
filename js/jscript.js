58dc5d43af209c447e9cfa6a28d1ed40866a9d0f
DejayJD <jdfrancis@smu.edu>
2015-04-10 23:03:03 -0500

    Accidentally pushed to wrong branch

--- js/jscript.js
+++ js/jscript.js
@@ -1,10 +1,9 @@
-angular.module('locations', []).controller('myCtrl', function($scope, $http){
-	console.log("got here");
-  	$http.get("loc-data.php")
-    	.success(function(response) {$scope.locations = response.data;});
-    $scope.changeMap = function(loc){
-  		console.log("clicked");
-  		L.marker(loc).addTo(map);
-  		map.setView(loc, 15);
-  	}
+$(function(){
+ 
+	$('.dlink').on('click', logConsole);
+
 });
+
+function logConsole() {
+	console.log("You clicked a link");
+}
