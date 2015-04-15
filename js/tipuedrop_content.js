6541f60e030203a8f2736056335330d8b89dfb26
Erik Gabrielsen <egabrielsen@smu.edu>
2015-04-14 23:02:19 -0500

    finally finished the changeHouse feature for ManagingSpots

--- js/tipuedrop_content.js
+++ js/tipuedrop_content.js
@@ -4,6 +4,5 @@ var tipuedrop = {"pages": [
      {"title": "Gerald Ford Stadium", "thumb": "img/search.png", "text": "The SMU Football Stadium. Go Mustangs!", "tags": "football", "loc": [32.83763, -96.78306]},
      {"title": "Another Event", "thumb": "img/drop.png", "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "tags": "JavaScript js", "loc": [31.83763, -96.78306]},
      {"title": "Another event", "thumb": "img/tipr.png", "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "tags": "Tipue", "loc": [33.83763, -96.78306]},
-     {"title":"Meadows Dance Recital","thumb":"img/drop.png","text":"a SPOTS suggested event","tags":"Dance","loc":[32.8411172, -96.7862027]},
-
+     
 ]};
