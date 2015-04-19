f662cbac9bee979e5399c7adaeae85dbeb5bd03f
Erik Gabrielsen <egabrielsen@smu.edu>
2015-04-19 00:16:40 -0500

    changes

--- signin/Homeowner/changePrice.php
+++ signin/Homeowner/changePrice.php
@@ -5,18 +5,18 @@
 		$newPrice = $_POST['newPrice'];
 		//connect to sql database
 		$conn = mysql_connect("localhost", "spotsuser", "spots123");
-
 		//choose database
 		$db = mysql_select_db("spots", $conn);
-
 		//Fetch info for users
-		$query = mysql_query("UPDATE Spots set price = $newPrice where homeId = $homeId", $conn);
-
+		$query = mysql_query("UPDATE Spots SET price = CASE 
+								WHEN taken = false THEN $newPrice 
+								ELSE price 
+							END
+							WHERE homeId = $homeId", $conn);
+		//should be able to just use a case statement in order to update the price
+		// will experiment with another database
 		mysql_close($conn);
-
 		header("Location: manageSpots.php");
 		
 	}
-
-
 ?>
