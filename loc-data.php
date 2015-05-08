14305f851c44a9d67481c801ed65b99017999981
Erik Gabrielsen <egabrielsen@smu.edu>
2015-05-07 21:36:39 -0500

    h

--- loc-data.php
+++
@@ -1,5 +0,0 @@
-{"data": 
-	[
-     {"title": "Gerald Ford Stadium", "thumb": "img/search.png", "text": "The SMU Football Stadium. Go Mustangs!", "tags": "football", "loc": [32.83763, -96.78306]}
-     ]
-}
